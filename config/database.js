4aac1608d2682bb50afe0ccca854e420974db823
dartfagnans <giuseppe.fagnani@studenti.unicam.it>
2018-06-29 16:48:07 +0200

    commit

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url' : '#####'
+    'url' : 'mongodb://localhost:3000/test'
 }
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var session = require ('express-session');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var configDB = require('./config/database.js');
 mongoose.connect(configDB.url);
@@ -15,13 +16,18 @@ app.use(cookieParser());
 app.use(session({secret: 'anystringoftext',
                 saveUninitialized: true,
                 resave: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', function(req, res) {
+app.set('view engine', 'ejs');
+
+/*app.use('/', function(req, res) {
     res.send('Our First Express program!');
     console.log(req.cookies);
     console.log('******************************');
     console.log(req.session);
-});
+});*/
+
+require('./app/routes.js')(app);
 
 app.listen(port, () => {
     console.log('Server running on port: ' + port);
